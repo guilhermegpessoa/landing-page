165b13087bc889e71aad5ac8229155521eae8aec
Guilherme Pessoa <guilherme.gpessoa@hotmail.com>
2025-08-17 20:48:31 -0300

    Add message bar at the top and update header/footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,11 +15,23 @@ export default function Header() {
 
   return (
     <header>
+      <div className="header-bar">
+        <p>
+          CENTRAL DE VENDAS: <img src="src/assets/iconPhone.png" />{' '}
+          <span> (31)4000-1667 </span>
+          <img src="src/assets/iconWhatsapp.png" /> <span>(31) 2534-2115</span>
+        </p>
+      </div>
       <nav className="container">
         <a href="#" className="logo">
           <img src={logo} alt="Logo" />
         </a>
         <NavMenu links={links} />
+        <div className="customer">
+          <a>
+            <img src="src/assets/iconCustomer.png" /> Área do Cliente &rarr;
+          </a>
+        </div>
       </nav>
     </header>
   );
